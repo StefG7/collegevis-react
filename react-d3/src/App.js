ddbd9659c9912f8421692fcf7cb58ad13aa3fb14
sgalive <stefanyarevalo76@gmail.com>
2023-02-03 18:51:50 -0800

    add all React stuff

---
+++ react-d3/src/App.js
@@ -0,0 +1,9 @@
+import BarChart from './BarChart'
+
+const App = () => {
+    return (<BarChart 
+        data={[12, 5, 6, 6, 9, 10]}
+        width={700}
+        height={300}/>)
+}
+export default App;
