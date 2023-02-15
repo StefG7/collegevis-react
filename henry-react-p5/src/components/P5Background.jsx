034c9863f24aa5b4c892ffec97f71558b5e160b3
Normand-1024 <hawtherabbit@gmail.com>
2023-02-14 18:15:45 -0800

    basic structure of landing page

--- henry-react-p5/src/components/P5Background.jsx
+++ henry-react-p5/src/components/P5Background.jsx
@@ -2,14 +2,36 @@ import React, {useEffect} from 'react';
 import p5 from 'p5';
 import './P5Background.css';
 
-import {UNI_COLORS, MAJOR_CATEGORIES, MAJOR_COLORS, EVENT_CODE} from '../constants.jsx';
+import {MAJOR_CATEGORIES, MAJOR_COLORS, MAJOR_POSITIONS, MINOR_CATEGORIES} from '../constants.jsx';
 
-let numBalls = MAJOR_CATEGORIES.length;
-let ballSizeRatio = 0.1;
-let spring = 0.05;
-let gravity = 0.03;
-let friction = -0.9;
-let balls = [];
+let landingPageState = 0; // 0: Major Planet in focus, 1: Minor Planet selection, 2: Minor Planet information
+let majorPlanetInFocus = -1; // id of the major planet clicked on
+
+let numPlanet = MAJOR_CATEGORIES.length;
+let planetSizeRatio = 0.15;
+let majorPlanetList = [];
+
+let majorMinorRatio = 0.2; // size ratio between major and minor planets
+let minorFocusRatio = 0.8; // size ratio between major and minor planets when the minor planets are in focus
+let minorDistRatio = 0.05; // the margin given between each in-focus minor planets
+let minorOrbitHeightRatio = 0.15; // how far from the major planet is the minor planet orbiting
+
+let borderMarginRatio = 0.05; // when in state 1, the ratio (to window size) of the margin from the border
+							 // Bigger the number, the closer to the center the planets are in state 2
+
+function projectPosition(p0, p1, left, right, top, bottom) {	
+	// Figure out planet position after putting a major planet in focus
+	// Cast a ray from p0 to p1, given four bounds, return intersection pt of the ray & the bounds
+	// p0 & p1: [x, y]; left & right & top & bottom: number; p: [x,y]
+	let slope = (p1[1] - p0[1]) / (p1[0] - p0[0]);
+	let intercept = p0[1] - p0[0] * slope;
+
+	let pp0 = p0[0] > p1[0] ? [left, left * slope + intercept] : [right, right * slope + intercept];
+	let pp1 = p0[1] > p1[1] ? [(top - intercept) / slope, top] : [(bottom - intercept) / slope, bottom];
+
+	if (pp0[1] >= top && pp0[1] <= bottom) return pp0;
+	else return pp1;
+}
 
 class P5Background extends React.Component {
 	constructor(props) {
@@ -23,13 +45,14 @@ class P5Background extends React.Component {
 		p5.setup = () => {
 			p5.createCanvas(p5.windowWidth, p5.windowHeight);
 
-			for (let i = 0; i < MAJOR_CATEGORIES.length; i++) {
-				balls[i] = new Ball(
-					p5.random(p5.width),
-					p5.random(p5.height),
-					p5.min(p5.windowWidth, p5.windowHeight) * ballSizeRatio,
+			for (let i = 0; i < numPlanet; i++) {
+				majorPlanetList[i] = new MajorPlanet(
+					p5.width * MAJOR_POSITIONS[MAJOR_CATEGORIES[i]][0],
+					p5.height * MAJOR_POSITIONS[MAJOR_CATEGORIES[i]][1],
+					p5.min(p5.windowWidth, p5.windowHeight) * planetSizeRatio,
 					i,
-					balls,
+					MAJOR_CATEGORIES[i],
+					majorPlanetList,
 					p5
 				);
 			}
@@ -39,17 +62,71 @@ class P5Background extends React.Component {
 		p5.draw = () => {
 			p5.background(0);
 
-			balls.forEach(ball => {
-				ball.collide();
-				ball.move();
-				ball.display();
+			majorPlanetList.forEach(planet => {
+				planet.display();
 			});
+		}
+
+		this.updatePlanetPosition = () => {
+			if (landingPageState == 0) {
+				for (let i = 0; i < majorPlanetList.length; i++){
+					majorPlanetList[i].x = p5.width * MAJOR_POSITIONS[MAJOR_CATEGORIES[i]][0];
+					majorPlanetList[i].y = p5.height * MAJOR_POSITIONS[MAJOR_CATEGORIES[i]][1];
+					majorPlanetList[i].diameter = p5.min(p5.windowWidth, p5.windowHeight) * planetSizeRatio;
+					majorPlanetList[i].updateMinorPlanetPosition();
+				}
+			}
+			else if (landingPageState == 1) {
+				let p0 = [p5.width * MAJOR_POSITIONS[MAJOR_CATEGORIES[majorPlanetInFocus]][0],
+						  p5.height * MAJOR_POSITIONS[MAJOR_CATEGORIES[majorPlanetInFocus]][1]];
+				let left = 0 + p5.width * borderMarginRatio;
+				let right = p5.width - p5.width * borderMarginRatio;
+				let top = 0 + p5.height * borderMarginRatio;
+				let bottom = p5.height - p5.height * borderMarginRatio;
+				
+				for (let i = 0; i < majorPlanetList.length; i++) {
+					let p1 = [p5.width * MAJOR_POSITIONS[MAJOR_CATEGORIES[i]][0],
+							p5.height * MAJOR_POSITIONS[MAJOR_CATEGORIES[i]][1]];
+
+					if (i != majorPlanetInFocus) {
+						[majorPlanetList[i].x, majorPlanetList[i].y] = projectPosition(p0, p1,
+																		left, right, top, bottom);
+						majorPlanetList[i].diameter = p5.min(p5.windowWidth, p5.windowHeight) * planetSizeRatio / 4;
+					}
+					else {
+						majorPlanetList[i].x = p5.width / 5;
+						majorPlanetList[i].y = p5.height / 3;
+						majorPlanetList[i].diameter = p5.min(p5.windowWidth, p5.windowHeight) * planetSizeRatio * 2;
+					}
+					majorPlanetList[i].updateMinorPlanetPosition();
+				}
+
+			}
 		}
 
 		p5.windowResized = () => {
 			p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
-			for (let i = 0; i < MAJOR_CATEGORIES.length; i++){
-				balls[i].diameter = p5.min(p5.windowWidth, p5.windowHeight) * ballSizeRatio;
+			this.updatePlanetPosition();
+		}
+
+		p5.mouseClicked = () => {
+			if (landingPageState == 0){
+				for (let i = 0; i < majorPlanetList.length; i++) {
+					if (p5.pow(p5.mouseX - majorPlanetList[i].x, 2) + 
+						p5.pow(p5.mouseY - majorPlanetList[i].y, 2) < 
+							p5.pow(majorPlanetList[i].diameter / 2, 2)) {
+						majorPlanetInFocus = i;
+						landingPageState = 1;
+						console.log(MAJOR_CATEGORIES[majorPlanetInFocus]);
+					}
+				}
+
+				this.updatePlanetPosition();
+			}
+			else if (landingPageState == 1){
+				landingPageState = 0;
+				
+				this.updatePlanetPosition();
 			}
 		}
 	 }
@@ -68,66 +145,119 @@ class P5Background extends React.Component {
 	}
 }
 
-class Ball {
-	constructor(xin, yin, din, idin, oin, p5) {
-	  this.x = xin;
-	  this.y = yin;
-	  this.vx = 0;
-	  this.vy = 0;
-	  this.diameter = din;
-	  this.id = idin;
-	  this.others = oin;
-	  this.p5 = p5;
-	}
-  
-	collide(){
-	  for (let i = this.id + 1; i < numBalls; i++) {
-		//console.log(this.others[i]);
-		let dx = this.others[i].x - this.x;
-		let dy = this.others[i].y - this.y;
-		let distance = this.p5.sqrt(dx * dx + dy * dy);
-		let minDist = this.others[i].diameter / 2 + this.diameter / 2;
-		//   console.log(distance);
-		//console.log(minDist);
-		if (distance < minDist) {
-		  //console.log("2");
-		  let angle = this.p5.atan(dy, dx);
-		  let targetX = this.x + this.p5.cos(angle) * minDist;
-		  let targetY = this.y + this.p5.sin(angle) * minDist;
-		  let ax = (targetX - this.others[i].x) * spring;
-		  let ay = (targetY - this.others[i].y) * spring;
-		  this.vx -= ax;
-		  this.vy -= ay;
-		  this.others[i].vx += ax;
-		  this.others[i].vy += ay;
+class MajorPlanet {
+	constructor(xin, yin, din, idin, nin, oin, p5) {
+		this.x = xin;
+		this.y = yin;
+		this.diameter = din;
+		this.id = idin;
+		this.name = nin;
+		this.others = oin;
+		this.p5 = p5;
+
+		this.minorList = [];
+		for (let i = 0; i < MINOR_CATEGORIES[this.name].length; i++) {
+			this.minorList[i] = new MinorPlanet(
+				xin,
+				yin,
+				din,
+				this.id,
+				i,
+				MINOR_CATEGORIES[this.name][i],
+				this.minorList,
+				p5
+			);
 		}
-	  }
 	}
-  
-	move(){
-	  this.vy += gravity;
-	  this.x += this.vx;
-	  this.y += this.vy;
-	  if (this.x + this.diameter / 2 > this.p5.width) {
-		this.x = this.p5.width - this.diameter / 2;
-		this.vx *= friction;
-	  } else if (this.x - this.diameter / 2 < 0) {
-		this.x = this.diameter / 2;
-		this.vx *= friction;
-	  }
-	  if (this.y + this.diameter / 2 > this.p5.height) {
-		this.y = this.p5.height - this.diameter / 2;
-		this.vy *= friction;
-	  } else if (this.y - this.diameter / 2 < 0) {
-		this.y = this.diameter / 2;
-		this.vy *= friction;
-	  }
+
+	updateMinorPlanetPosition() {
+		for (let i = 0; i < this.minorList.length; i++) {
+			this.minorList[i].majorX = this.x;
+			this.minorList[i].majorY = this.y;
+			this.minorList[i].majorD = this.diameter;
+			this.minorList[i].r = this.diameter / 2 + this.diameter * minorOrbitHeightRatio;
+			this.minorList[i].setPosition();
+		}
 	}
-  
+
 	display(){
-	  this.p5.fill(MAJOR_COLORS[MAJOR_CATEGORIES[this.id]]);
-	  this.p5.ellipse(this.x, this.y, this.diameter, this.diameter);
+		if (landingPageState == 0){
+			// Hover checking
+			if (this.p5.pow(this.p5.mouseX - this.x, 2) + 
+				this.p5.pow(this.p5.mouseY - this.y, 2) < 
+					this.p5.pow(this.diameter / 2, 2)) {
+				this.p5.fill("#ffffff");
+			}
+			else {
+				this.p5.fill(MAJOR_COLORS[MAJOR_CATEGORIES[this.id]]);
+			}
+		}
+		else this.p5.fill(MAJOR_COLORS[MAJOR_CATEGORIES[this.id]]);
+
+		this.p5.ellipse(this.x, this.y, this.diameter, this.diameter);
+
+		for (let i = 0; i < this.minorList.length; i++) {
+			this.minorList[i].display();
+		}
 	}
-  }
+}
+
+class MinorPlanet {
+	constructor(xin, yin, din, midin, idin, nin, oin, p5) {
+		this.majorX = xin;
+		this.majorY = yin;
+		this.majorD = din;
+		this.majorI = midin;
+		this.id = idin;
+		this.name = nin;
+		this.others = oin;
+		this.p5 = p5;
+
+		this.r = this.majorD / 2 + this.majorD * minorOrbitHeightRatio;
+		this.angle = this.id * this.p5.PI / 4;
+		this.setPosition();
+	}
+
+	setPosition(){
+		let x = 0;
+		let y = 0;
+		let d = this.majorD * majorMinorRatio;
+		let m = this.majorD * minorDistRatio;
+		if (landingPageState == 1 && this.majorI == majorPlanetInFocus){
+			d = this.majorD * minorFocusRatio;
+
+			// Constrain d by screen width
+			let d_width = this.p5.width * (1 - borderMarginRatio * 2) / this.others.length;
+			d = d > d_width ? d_width : d;
+
+			x = this.p5.width / 2 + (this.others.length - 1) * (-0.5 * d - 0.5 * m) + this.id * (d + m); // left-most position + id * increments
+			y = this.p5.height * 2 / 3;
+		}
+		else {
+			x = this.majorX + this.r * this.p5.cos(this.angle);
+			y = this.majorY - this.r * this.p5.sin(this.angle);
+		}
+		this.x = x;
+		this.y = y;
+		this.d = d;
+	}
+
+	display(){
+		if (landingPageState == 1){
+			// Hover checking
+			if (this.p5.pow(this.p5.mouseX - this.x, 2) + 
+				this.p5.pow(this.p5.mouseY - this.y, 2) < 
+					this.p5.pow(this.d / 2, 2)) {
+				this.p5.fill("#ffffff");
+			}
+			else {
+				this.p5.fill(MAJOR_COLORS[MAJOR_CATEGORIES[this.majorI]]);
+			}
+		}
+		else this.p5.fill(MAJOR_COLORS[MAJOR_CATEGORIES[this.majorI]]);
+
+		this.p5.ellipse(this.x, this.y, this.d, this.d);
+	}
+}
 
 export default P5Background;
--- henry-react-p5/src/constants.jsx
+++ henry-react-p5/src/constants.jsx
@@ -4,7 +4,6 @@ export const UNI_COLORS = {
     'University of California-Irvine': '#cc0066',
     'University of California-Los Angeles': '#66cccc',
     'University of California-Merced': '#f688bb',
-    'University of California-Berkeley': '#65587f',
     'University of California-Riverside': '#baf1a1',
     'University of California-San Diego': '#0c7b93',
     'University of California-Santa Barbara': '#75b79e',
@@ -24,6 +23,60 @@ export const MAJOR_CATEGORIES = [
     "Mathematics & Economics"
 ]
 
+export const MINOR_CATEGORIES = {
+    "Agriculture & Environmental Studies": 
+        ["Agriculture & Food Science",
+         "Environmental Science & Studies",
+         "Animal Science"],
+    "Arts & Design": 
+        ["General Art",
+         "Design",
+         "Music",
+         "Cinema & Visual Arts",
+         "Performing Arts"],
+    "Human & Public Services": 
+        ["Education",
+         "Law & Criminology",
+         "Social Services"],
+    "Natural Sciences": 
+        ["Earth Sciences",
+         "Physics & Astronomy",
+         "Biology",
+         "Chemistry"],
+    "Engineering & Technology": 
+        ["Information & Technology",
+         "Specializations & Engineering",
+         "Electrical & Mechanical Engineering",
+         "Computer Science and Engineering",
+         "City Planning & Architecture"],
+    "Humanities": 
+        ["Cultural & Ethnic Studies",
+         "Feminist & Gender Studies",
+         "Philosophy & Religion",
+         "Language & Literature",
+         "Anthropology & History",
+         "General Humanities",
+         "Communication & Journalism"],
+    "Social & Political Sciences": 
+        ["Politics & Public Policy",
+         "Sociology",
+         "Linguistics"],
+    "Business & Management": 
+        ["Management & Information Systems",
+         "Accounting & Finance",
+         "Marketing & Consumer Services",
+         "Specializations & Business"],
+    "Health & Psychology": 
+        ["Food & Nutrition",
+         "Psychology",
+         "Medical Sciences",
+         "Athletic & Bodily Science"],
+    "Mathematics & Economics": 
+        ["Statistics & Data Science",
+         "Applied & General Mathematics",
+         "Economics"]
+}
+
 export const MAJOR_COLORS = {
     "Agriculture & Environmental Studies": '#d175ff',
     "Arts & Design": '#ffb3b7',
@@ -37,6 +90,15 @@ export const MAJOR_COLORS = {
     "Mathematics & Economics": '#75b79e'
 }
 
-export const EVENT_CODE = {
-    "bubble_click_update": 0
+export const MAJOR_POSITIONS = {
+    "Agriculture & Environmental Studies": [0.22576468, 0.78682877],
+    "Arts & Design": [0.62719317, 0.46743662],
+    "Human & Public Services": [0.12048579, 0.44192509],
+    "Natural Sciences": [0.76058128, 0.81376119],
+    "Engineering & Technology": [0.76394922, 0.19356788],
+    "Humanities": [0.509, 0.21],
+    "Social & Political Sciences": [0.23993193, 0.1631277],
+    "Business & Management": [0.86995319, 0.5784954],
+    "Health & Psychology": [0.3584272 , 0.49914542],
+    "Mathematics & Economics": [0.49405762, 0.79226853]
 }
