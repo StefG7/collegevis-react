07685c2336383c36de70d9e4457d77bbadb4a02e
sgalive <stefanyarevalo76@gmail.com>
2023-02-24 22:37:52 -0800

    add base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/collegevis-react/',
   server:  {
     port: 8000,
   },
