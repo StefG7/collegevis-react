79789772735e4b24522fa7daceece6a140d32485
sgalive <stefanyarevalo76@gmail.com>
2023-03-17 18:41:42 -0700

    Update App.jsx
    Open to home page for fair submission

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css'
 import {PAGE_STATE} from './constants.jsx';
 
 function App() {
-    const [state, setState] = useState(PAGE_STATE["Careers"]);//"Majors");
+    const [state, setState] = useState(PAGE_STATE["Home"]);//"Majors");
     const [minorSelections, setMinorSelections] = useState([]);
 
     return (
